135aa2b90c9c7b6bed771ec0439c494cec4c681c
defsub <defsub@defsub.com>
2020-04-03 15:45:09 -0700

    license

--- encoding/spiff/spiff.go
+++ encoding/spiff/spiff.go
@@ -1,3 +1,20 @@
+// Copyright (C) 2020 The Takeout Authors.
+//
+// This file is part of Takeout.
+//
+// Takeout is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// Takeout is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Takeout.  If not, see <https://www.gnu.org/licenses/>.
+
 package spiff
 
 import (
@@ -50,7 +67,7 @@ func (e *Encoder) Encode(track interface{}) (err error) {
 
 	t := reflect.TypeOf(track)
 	v := reflect.ValueOf(track)
-	
+
 	switch v.Kind() {
 	case reflect.Struct:
 		for i := 0; i < t.NumField(); i++ {
