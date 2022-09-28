766d32aca7af830a2df00b90644b7f2642e8730e
defsub <defsub@defsub.com>
2022-09-28 16:31:21 -0700

    301 img

--- server/image.go
+++ server/image.go
@@ -32,45 +32,45 @@ func imgRelease(w http.ResponseWriter, r *http.Request) {
 	reid := r.URL.Query().Get(":reid")
 	side := r.URL.Query().Get(":side")
 	url := fmt.Sprintf("%s/release/%s/%s-250", CoverArtArchivePrefix, reid, side)
-	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+	http.Redirect(w, r, url, http.StatusMovedPermanently)
 }
 
 func imgReleaseFront(w http.ResponseWriter, r *http.Request) {
 	reid := r.URL.Query().Get(":reid")
 	url := fmt.Sprintf("%s/release/%s/front-250", CoverArtArchivePrefix, reid)
-	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+	http.Redirect(w, r, url, http.StatusMovedPermanently)
 }
 
 func imgReleaseGroup(w http.ResponseWriter, r *http.Request) {
 	rgid := r.URL.Query().Get(":rgid")
 	side := r.URL.Query().Get(":side")
 	url := fmt.Sprintf("%s/release-group/%s/%s-250", CoverArtArchivePrefix, rgid, side)
-	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+	http.Redirect(w, r, url, http.StatusMovedPermanently)
 }
 
 func imgReleaseGroupFront(w http.ResponseWriter, r *http.Request) {
 	rgid := r.URL.Query().Get(":rgid")
 	url := fmt.Sprintf("%s/release-group/%s/front-250", CoverArtArchivePrefix, rgid)
-	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+	http.Redirect(w, r, url, http.StatusMovedPermanently)
 }
 
 func imgVideo(w http.ResponseWriter, r *http.Request) {
 	size := r.URL.Query().Get(":size")
 	path := r.URL.Query().Get(":path")
 	url := fmt.Sprintf("%s/t/p/%s/%s", TMDBPrefix, size, path)
-	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+	http.Redirect(w, r, url, http.StatusMovedPermanently)
 }
 
 func imgArtistThumb(w http.ResponseWriter, r *http.Request) {
 	arid := r.URL.Query().Get(":arid")
 	path := r.URL.Query().Get(":path")
 	url := fmt.Sprintf("%s/music/%s/artistthumb/%s", FanArtPrefix, arid, path)
-	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+	http.Redirect(w, r, url, http.StatusMovedPermanently)
 }
 
 func imgArtistBackground(w http.ResponseWriter, r *http.Request) {
 	arid := r.URL.Query().Get(":arid")
 	path := r.URL.Query().Get(":path")
 	url := fmt.Sprintf("%s/music/%s/artistbackground/%s", FanArtPrefix, arid, path)
-	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+	http.Redirect(w, r, url, http.StatusMovedPermanently)
 }
